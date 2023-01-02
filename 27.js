85175c5d3174101ea9ccc79f970a33536ab668c0
Ashmit Mehta <amehta.ashmit2@gmail.com>
2023-01-02 10:25:11 +0530

    Alert,prompt, and confirm

---
+++ 27.js
@@ -0,0 +1,10 @@
+alert("This is an alert")
+alert("Enter a value")
+let a =prompt("This is an prompt")
+let b=confirm("Please confirm")
+if(b){
+    document.write("The user has made the confirmation")
+}
+else{
+    document.write("The user has denied the confirmation")
+}
